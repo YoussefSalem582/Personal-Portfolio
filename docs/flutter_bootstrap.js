875863cb8cc37ce7b83a158055263683ecaa9b5f
Youssef Hassan <youssef.salem.hassan582@gmail.com>
2025-10-21 23:37:14 +0300

    fix: Add correct base-href for GitHub Pages deployment

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2240413053"
+    serviceWorkerVersion: "2997789925"
   }
 });
